cf9dbc14998ebb3b335f9a1e4530397cc6ff0bdc
tmenture <thomasoxemail@gmail.com>
2022-07-14 20:33:51 -0400

    fixed logged errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
-    './css/style.css',
+    './css/styles.css',
     './js/index.js',
     './js/db.js',
     './manifest.json',
@@ -21,7 +21,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache: ' + CACHE_NAME);
+            console.log('installing cache : ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
     )
@@ -29,15 +29,15 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        cahces.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
-            cacheKeepList.push(CACHE_NAME);
+            cacheKeeplist.push(CACHE_NAME);
 
             return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeepList.indexOf(key) === -1) {
-                    console.log('deleting cache: ' + keyList[i]);
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
                     return caches.delete(keyList[i]);
                 }
             }));
@@ -46,16 +46,17 @@ self.addEventListener('activate', function (e) {
 });
 
 self.addEventListener('fetch', function (e) {
-    console.log('fetch request: ' + e.request.url);
+    console.log('fetch request : ' + e.request.url);
     e.respondWith(
         caches.match(e.request).then(function (request) {
             if (request) {
-                console.log('responding with cache: ' + e.request.url);
-                return request;
+                console.log('responding with cache : ' + e.request.url);
+                return request
             } else {
-                console.log('file is not cached, fetching: ' + e.request.url);
-                return fetch(e.request);
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
             }
+
         })
     )
 });
